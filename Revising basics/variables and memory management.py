2ec88a807bb90d0a6412916283d5a72aa280aac1
Santosh Reddy Uppada <119440226+santoshreddy6@users.noreply.github.com>
2026-06-24 18:58:31 +0530

    practicing variable and memory management examples
    Add examples of variable types and memory management.

---
+++ Revising basics/variables and memory management.py
@@ -0,0 +1,22 @@
+# Integer
+age = 20
+print(type(age))
+print(age)
+
+# Float
+height = 5.8
+print(type(height))
+print(height)
+
+# String
+name = "Rahul"
+print(type(name))
+print(name)
+
+# Boolean
+is_student = True
+print(type(is_student))
+print(is_student)
+
+print(id(age))
+print(id(height))
